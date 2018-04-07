a9e825dab5b1a20fbf0690ca7a05029556a90895
yuzhengDL <18811435811@163.com>
2018-04-07 14:43:51 +0800

    Update fetch_and_preprocess.sh

--- Dependencey_Parser/fetch_and_preprocess.sh
+++ Dependencey_Parser/fetch_and_preprocess.sh
@@ -3,8 +3,4 @@ set -e
 #python2.7 scripts/download.py
 CLASSPATH="lib:lib/stanford-parser/stanford-parser.jar:lib/stanford-parser/stanford-parser-3.5.1-models.jar"
 javac -cp $CLASSPATH lib/*.java
-java -cp $CLASSPATH DependencyParserDemo
-#python2.7 scripts/preprocess-sst.py
-#python2.7 scripts/glove_compress.py
-#rm ./data/glove/glove.840B.300d.txt
-#touch ./data/glove/glove.840B.300d.txt
+python scripts/preprocess-f8k.py
